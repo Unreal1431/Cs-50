76c6ddbdbe73ca786c9a009687d3e447049b7589
Stein <unrealtournament2007@rambler.rugit config --global user.email unrealtournament2007@rambler.rugit config --global user.email unrealtournament2007@rambler.ru>
2017-12-26 03:43:13 +0400

    26.12.17

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Pyramid
+{
+
+    class Program
+    {
+        static void pyramid(int n)
+        {
+           
+            for (int z = 1; z < n; z++)
+            Console.Write(" ");
+            Console.Write("*");
+            Console.WriteLine("");
+            if (n > 1)
+                for (int i = 1; i < n; i++)
+                {
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int k = n - i; k < n; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.Write(" ");
+                    for (int k = n - i; k < n; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine("");
+                }
+        }
+        static void Main(string[] args)
+        {
+            int write = 1;
+            while (write != 0)
+            {
+                write = int.Parse(Console.ReadLine());
+                pyramid(write);
+            }
+        }
+    }
+}

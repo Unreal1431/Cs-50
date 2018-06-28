0349f025d77389e5aa6d7adbfe29579d152ad0e2
Unreal1431 <32221399+Unreal1431@users.noreply.github.com>
2018-06-28 23:04:59 +0400

    variant 2
    add pyramid variant 2

---
+++ Program2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication25
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            int piramyd = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= piramyd; i++)
+            {
+
+                Console.Write(new string(' ', piramyd - i));
+                Console.Write(new string('*', i));
+                Console.Write(' ');
+                Console.Write(new string('*', i));
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
